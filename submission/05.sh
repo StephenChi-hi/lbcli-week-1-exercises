ee0bee24dc13bcd7ed528c48989e596ab75540ad
StephenChi-hi <nwikechisom77@gmail.com>
2026-03-18 05:24:03 +0100

    fix 05 total balance

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,4 @@
 # Check the total amount in the wallet.
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS > /dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
